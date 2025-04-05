412744e9defd8f21449b0c8df863e8f3025657bf
gustavoDecker <gustavo2005@uniplaclages.edu.br>
2025-04-05 12:08:22 -0300

    Atividade 2

---
+++ script.js
@@ -0,0 +1,9 @@
+var varString = "Meu nome é Gustavo";
+var varInteiro = 19;
+var varFloat = 19.4;
+var varBooleano = false;
+
+alert ("String: " + varString);
+alert ("Inteiro: " + varInteiro);
+alert ("Float: " + varFloat);
+alert ("Booleano: " + varBooleano);
